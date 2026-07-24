5adea415b5d48f7777c63108f613e6d25d38b09d
HiyokoPvp <harukikanamari@gmail.com>
2026-07-24 16:58:43 +0900

    123

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -23,33 +23,7 @@ local getAccountTier = function(fuck)
 	end	
 end	
 
-local function acinit()
-    local success, err = pcall(function()
-        assert(getgc, "executor missing required function getgc")
-        assert(debug.info, "executor missing required function debug.info")
-        assert(hookfunction, "executor missing required function hookfunction")
-        assert(getconnections, "executor missing required function getconnections")
 
-        for _, v in getgc() do
-            if typeof(v) == "function" then
-                local source = debug.info(v, "s")
-                if source and string.find(source, "analytics-controller", 1, true) then
-                    hookfunction(v, function()
-                        return task.wait(9e9)
-                    end)
-                end
-            end
-        end
-    end)
-
-    if not success then
-        print("ごみちーとしね: " .. tostring(err))
-    else
-        print("ばいぱすせいこうｗｗｗ")
-    end
-end
-
-acinit()
 
 local playersService = cloneref(game:GetService('Players'))
 local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
