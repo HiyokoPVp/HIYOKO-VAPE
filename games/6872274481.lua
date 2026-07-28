1c5d88b290db5dd0ce53d4e1eca8cb142374de97
HiyokoPvp <harukikanamari@gmail.com>
2026-07-28 23:30:09 +0900

    123

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -796,9 +796,9 @@ end
 run(function()
 	local KnitInit, Knit
 	repeat
-		KnitInit, Knit = pcall(function()
-			return debug.getupvalue(require(lplr.PlayerScripts.TS.knit).setup, 9)
-		end)
+    KnitInit, Knit = pcall(function()
+       return require(replicatedStorage.rbxts_include.node_modules["@easy-games"].knit.src).KnitClient
+    end)
 		if KnitInit then break end
 		task.wait()
 	until KnitInit
