e5c6ad1b6f20ef27bfbe10a2437144060b3db919
HiyokoPvp <harukikanamari@gmail.com>
2026-07-29 18:00:28 +0900

    123

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -19273,7 +19273,7 @@ run(function()
                     -- 10秒ごとにスピードを上昇
                     if tick() - lastIncrease >= 10 then
                         lastIncrease = tick()
-                        currentSpeed = math.min(currentSpeed + 2, 85) -- 最大60studs
+                        currentSpeed = math.min(currentSpeed + 0.5, 85) -- 最大60studs
                         
                         -- 60studsに達するまで "Keep moving" を通知
                         if currentSpeed < 60 then
