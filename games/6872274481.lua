d60704408ece73b7c7cafcd9572f28f6a02a5dfb
HiyokoPvp <harukikanamari@gmail.com>
2026-07-29 22:52:46 +0900

    123

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -784,9 +784,14 @@ run(function()
 	vape:Clean(entitylib.Events.LocalAdded:Connect(updateVelocity))
 end)
 entitylib.start()
-local function safeGetProto(...)
-		local success, res = pcall(debug.getproto, ...)
-		return success and res or function() end
+local function safeGetProto(func, index)
+	if not func then return nil end
+	local success, proto = pcall(debug.getproto, func, index) -- ★debug.getproto に修正
+	if success then
+		return proto
+	else
+		return nil
+	end
 end
 run(function()
 	local KnitInit, Knit
